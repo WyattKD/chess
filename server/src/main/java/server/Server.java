3c1d866542a1e00a56621ad2f282715a8cdb10d6
WyattKD <wykd47@gmail.com>
2026-02-26 14:08:57 -0700

    Implemented clear endpoint

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,9 +1,24 @@
 package server;
 
+import com.google.gson.Gson;
+import dataaccess.*;
 import io.javalin.Javalin;
-import model.UserData;
+import io.javalin.http.Context;
+import org.jetbrains.annotations.NotNull;
+import service.ClearService;
+import service.GameService;
+import service.UserService;
 
 public class Server {
+
+
+    AuthDAO authDAO;
+    GameDAO gameDAO;
+    UserDAO userDAO;
+
+    static ClearService clearService;
+    static GameService gameService;
+    static UserService userService;
 
     private final Javalin javalin;
 
@@ -11,12 +26,35 @@ public class Server {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
+        try {
+
+            authDAO = new MemoryAuthDAO();
+            gameDAO = new MemoryGameDAO();
+            userDAO = new MemoryUserDAO();
+
+            clearService = new ClearService(authDAO, gameDAO, userDAO);
+            //gameService = new GameService();
+            //userService = new UserService();
+        } catch (Exception exception) {
+            System.out.println(exception);
+        }
+        
+        javalin.delete("/db", this::clearHandler);
+
+    }
+
+    private Object clearHandler(@NotNull Context context) throws DataAccessException {
+        clearService.clearDatabase();
+        context.status(200);
+        return "{}";
     }
 
     public int run(int desiredPort) {
         javalin.start(desiredPort);
         return javalin.port();
     }
+
+
 
     public void stop() {
         javalin.stop();
--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -11,7 +11,7 @@ public class ClearService {
     private final UserDAO userDAO;
 
 
-    public ClearService(AuthDAO authDAO,GameDAO gameDAO, UserDAO userDAO) {
+    public ClearService(AuthDAO authDAO, GameDAO gameDAO, UserDAO userDAO) {
         this.authDAO = authDAO;
         this.gameDAO = gameDAO;
         this.userDAO = userDAO;
